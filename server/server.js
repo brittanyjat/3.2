41723bf35c838506d39095d81899c183f247b268
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-02-20 16:54:21 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express')
+    , app = express()
+    , bodyParser = require('body-parser')
+    , session = require('express-session')
+    , passport = require('passport')
+    , Auth0Strategy = require('passport-auth0');
+
+const { SERVER_PORT, SECRET, AUTH_DOMAIN, AUTH_CLIENT_ID, AUTH_CLIENT_SECRET, AUTH_CALLBACK_URL } = process.env;
+
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+passport.use(new Auth0Strategy({
+    domain: AUTH_DOMAIN,
+    clientID: AUTH_CLIENT_ID,
+    clientSecret: AUTH_CLIENT_SECRET,
+    callbackURL: AUTH_CALLBACK_URL,
+    scope: 'openid profile'
+}, function (accessToken, refreshToken, extraParams, profile, done) {
+    // console.log(profile);
+    done(null, profile)
+}));
+
+passport.serializeUser((profile, done) => {
+    done(null, profile);
+})
+
+passport.deserializeUser((profile, done) => {
+    done(null, profile);
+})
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/',
+    failureRedirect: '/auth'
+}));
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Hello, Brittany. Coming at you live from ${SERVER_PORT}!`)
+})
---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <a href={process.env.REACT_APP_LOGIN}><button>Login</button></a>
+      </div>
+    );
+  }
+}
+
+export default App;
